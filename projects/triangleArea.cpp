dd85649ce084df06bc47d6d97158cee2abf46824
OmerFG_ <46255917+omerfaruk08@users.noreply.github.com>
2023-05-21 12:51:23 +0300

    tArea.cpp
    Triangle area calculation.

---
+++ projects/triangleArea.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    double bTriangle,hTriangle,triangleArea;
+    
+
+    cout << endl << "*-*-*-*-*-*-*" << endl;
+    cout << "Enter height length of your triangle : ";
+    cin >> hTriangle;
+    cout << endl;
+
+    cout << "Enter base length of your triangle : ";
+    cin >> bTriangle;
+    cout << endl;
+
+    triangleArea = (bTriangle * hTriangle)/2 ;
+
+    cout << "Your mesauraments are {height = " << hTriangle << " base = " << bTriangle << "} " << endl
+         << "Area of your triangle is == " << triangleArea  ;
+
+    cout << endl << "*-*-*-*-*-*-*" << endl;
+
+    return 0;
+}
