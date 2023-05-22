d79cb14d914544e7a4d65d0ad974fdc7d53da3d4
OmerFG_ <46255917+omerfaruk08@users.noreply.github.com>
2023-05-22 20:15:26 +0300

    Create PalindromeWords.cpp
    Palindrome word detecting using c++.

---
+++ projects/PalindromeWords.cpp
@@ -0,0 +1,50 @@
+#include <iostream> 
+#include <string>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+
+int main(){
+
+    string wordInp , reversedInp;
+
+    cout << "Enter a word: ";
+    getline(cin,wordInp);
+
+
+
+    //deleting whitespaces
+    wordInp.erase(remove(wordInp.begin(),wordInp.end(),' '),wordInp.end());
+
+
+
+    //Converting big char's to little.
+    transform(wordInp.begin(), wordInp.end(), wordInp.begin() , [](unsigned char c) { return tolower(c); });
+    
+
+
+    //Reverse.
+    reversedInp = wordInp;
+    reverse(reversedInp.begin(), reversedInp.end());
+
+
+
+
+    if(reversedInp == wordInp){
+
+        cout << endl << "Your word is a palindrome word or sentence.";
+
+
+    }else{
+
+        cout << endl << "Your word isn't a palindrome word or sentence.";
+
+    }
+
+    cout << endl;
+
+
+
+    return 0;
+}
